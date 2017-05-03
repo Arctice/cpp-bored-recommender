7f55a0ffc29485b80d847d5e41a15fa35df95c60
arc <naurthal0@gmail.com>
2017-05-03 14:10:29 +0200

    cleanup
    linux builds work fine now

--- unix/cpp_bored/source/data_store.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#pragma comment(lib,"WS2_32")
-#include <cpp_redis/cpp_redis>
-using namespace std;
-using redis = cpp_redis::redis_client;
-
-shared_ptr<redis> redis_connect();
--- unix/cpp_bored/source/recommendations.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "user_data.h"
-
-using media_values = vector<pair<media_id, double>>;
-
-map<media_type, media_values> recommendations
-	(const string& name, redis& data_store);

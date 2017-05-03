989ff8cd0597dccc99969e29e905c3615f316a18
arc <naurthal0@gmail.com>
2017-05-03 12:59:55 +0200

    worker code

---
+++ vs/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define RECOMMENDATION_QUEUE "recommendation_queue"
+#define RECOMMENDATIONS_RAW "recommendations"
+#define NEW_RECOMMENDATIONS "recently_processed"
+
--- vs/main.cpp
+++ vs/main.cpp
@@ -1,26 +1,90 @@
+#include "config.h"
 #include "data_store.h"
 #include "recommendations.h"
 #include <chrono>
-using namespace std::chrono;
+
+// seconds since epoch, hopefully portable, hopefully won't take us back in time
+auto time(){
+	return std::chrono::duration_cast<std::chrono::seconds>
+		(std::chrono::system_clock::now().time_since_epoch())
+		.count();
+}
+
+// lazy
+string jsonify(const map<media_type, media_values>& recs){
+	string json = "{\"anime\": [";
+
+	constexpr auto MAX_RECS = 25;
+
+	for(int i = 0; i<MAX_RECS; ++i){
+		auto id = recs.at(ANIME)[i].first.second;
+		json += to_string(id);
+		if(i<MAX_RECS-1)
+			json += ", "s;
+	}
+
+	json += "], \"manga\": ["s;
+
+	for(int i = 0; i<MAX_RECS; ++i){
+		auto id = recs.at(MANGA)[i].first.second;
+		json += to_string(id);
+		if(i<MAX_RECS-1)
+			json += ", "s;
+	}
+
+	json += "]}"s;
+
+	return json;
+}
+
+void recommendations_job(const string& job, redis& data_store){
+	auto recs = recommendations(job, data_store);
+	
+	auto json = jsonify(recs);
+
+	cout<<"done "<<job<<endl;
+	data_store.hset(RECOMMENDATIONS_RAW, job, json);
+	data_store.rpop(RECOMMENDATION_QUEUE);
+	data_store.lpush(NEW_RECOMMENDATIONS, job);
+	data_store.sync_commit();
+}
 
 int main(){
+	string queue = RECOMMENDATION_QUEUE;
+
 	auto r = redis_connect();
 
+	while(true){
+		string job;
+		r->lindex(queue, -1,
+			[&](cpp_redis::reply& reply){
+				if(reply.is_null())
+					job = "";
+				else
+					job = reply.as_string();
+			});
+		r->sync_commit();
 
-	auto start = high_resolution_clock::now();
+		if(job.empty()){
+			// sleep 2s
+			continue;
+		}
 
-	auto z = recommendations("rainlife", *r);
+		cout<<"recommendation queue tail: ";
 
-	auto finish = high_resolution_clock::now();
-	cout<<duration_cast<milliseconds>(finish-start).count()<<endl;
-
-	cout<<"anime\n";
-	for(int i = 0; i<10; i++)
-		cout<< z[ANIME][i].first.second <<": "<<z[ANIME][i].second<<endl;
-
-	cout<<"\nmanga\n";
-	for(int i = 0; i<10; i++)
-		cout<<z[MANGA][i].first.second <<": "<<z[MANGA][i].second<<endl;
+		r->lrange(queue, -8, -1, [](cpp_redis::reply& reply){
+			if(reply.is_null()) cout<<"[]";
+			else{
+				for(const auto& elem : reply.as_array())
+					cout<<elem.as_string()+", ";
+			}
+			cout<<endl;
+		});
+		r->sync_commit();
+		cout<<"processing "<<job<<endl;
+		
+		recommendations_job(job, *r);
+	}
 
 	cin.get();
 }
